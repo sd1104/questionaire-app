f80ccb6df2c786003a95dec3221ca0aa0a77d123
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-22 17:51:17 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.common')
 @section('content')
-
-
+  <br><br>
+  <div class="container">
   <div class="hello">
     hello new world
+    <button type="button" class="btn btn-primary">Primary</button>
   </div>
-  <button type="button" class="btn btn-primary">Primary</button>
-
+  </div>
 
 @endsection
