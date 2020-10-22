8b5790f5e6c24762ede08213b36054ada89273b2
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-22 18:51:02 +0900

    Create footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+@section('footer')
+  <footer>
+    <div class="nav footer_nav">
+      <ul class="footer_links">
+        <li><a href="#">会員規約</a></li>
+        <li><a href="#">プライバシーステートメント</a></li>
+        <li><a href="#">個人情報保護方針</a></li>
+        <li><a href="#">サイト情報</a></li>
+      </ul>
+      <div class="copyright">copyright sd1104</div>
+    </div>
+  </footer>
+@show
