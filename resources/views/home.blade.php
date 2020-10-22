641c6eb281dc0e812fc0057f6d94585124518c6a
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-22 14:50:42 +0900

    common tenplate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href='../css/reset.css'>
-  <title>Document</title>
-</head>
-<body>
+@extends('layouts.common')
+
+@section('content')
   <div class="hello">
-    Hello new PHP
+    hello new world
   </div>
-
-
-</body>
-</html>
+@endsection
