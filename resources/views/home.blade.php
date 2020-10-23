682c5a52a6504e756d7e30c003edd77b0a9c922c
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-23 18:26:16 +0900

    register

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
         <span class="top_copy_sub">子どもたちのよりよい成長のために</span>
       </p>
       <div class="top_image_author">
-        <a class="author" href="https://pixabay.com/ja/users/qimono-1962238/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3255136">Arek Socha</a>
+        <a class="author" href="https://pixabay.com/ja/users/geralt-9301/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4039486">Gerd Altmann</a>
         による<br>
         <a class="author" href="https://pixabay.com/ja/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3255136">Pixabay</a>
         からの画像
@@ -50,38 +50,58 @@
 
     <div class="reason_contents">
       <div class="reason_explanation">
-          <span class="reason_explain_head">何のためにアンケートをするの？</span><br>
-          <span class="reason_explain_body">
-            この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
-            この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
-            この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
-            この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
-            この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
-          </span>
-        </div>
-        <ul class="reason_images">
-          <li class="reason_image">
-            <p class="reason_head">授業を改善するため</p>
-            <div id="reason_image1"></div>
-          </li>
-          <li class="reason_image">
-            <p class="reason_head">行事を精選するため</p>
-            <div id="reason_image2"></div>
-          </li>
-          <li class="reason_image">
-            <p class="reason_head">学習効果をより客観的に知るため</p>
-            <div id="reason_image3"></div>
-          </li>
-          <li class="reason_image">
-            <p class="reason_head">無駄を減らすため</p>
-            <div id="reason_image4"></div>
-          </li>
-        </ul>
+        <span class="reason_explain_head">何のためにアンケートをするの？</span><br>
+        <span class="reason_explain_body">
+          この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
+          この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
+          この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
+          この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
+          この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
+        </span>
       </div>
+      <ul class="reason_images">
+        <li class="reason_image">
+          <p class="reason_head">授業を改善するため</p>
+          <div id="reason_image1"></div>
+        </li>
+        <li class="reason_image">
+          <p class="reason_head">行事を精選するため</p>
+          <div id="reason_image2"></div>
+        </li>
+        <li class="reason_image">
+          <p class="reason_head">学習効果をより客観的に知るため</p>
+          <div id="reason_image3"></div>
+        </li>
+        <li class="reason_image">
+          <p class="reason_head">無駄を減らすため</p>
+          <div id="reason_image4"></div>
+        </li>
+      </ul>
     </div>
 
 
-    <div class="register_way_contens"></div>
-    <div class="advantage_contents"></div>
+    <div class="register_way_contens">
+      <div class="register_explanation">
+        <span class="register_explain_head">登録からご利用までの流れ</span><br>
+      </div>
+      <ul class="register_images">
+        <li class="register_image">
+          <p class="register_head">新規登録</p>
+          <div id="register_image1"></div>
+        </li>
+        <li class="register_image">
+          <p class="register_head">アンケートを選択</p>
+          <div id="register_image2"></div>
+        </li>
+        <li class="register_image">
+          <p class="register_head">回答</p>
+          <div id="register_image3"></div>
+        </li>
+      </ul>
+    </div>
+
+
+
+
   </div>
 @endsection
