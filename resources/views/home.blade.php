434414a7db11e5a8d57c7b39ec6b342bf235a30b
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-23 16:36:22 +0900

    top-image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.common')
 @section('content')
   <div class="wrapper">
-    <div class="top_image"></div>
+    <div id="top_image">
+      <div id="top_icon"></div>
+      <p class="top_copy">
+        <span class="top_copy_main">学校運営のための</span><br>
+        <span class="top_copy_main">アンケートサイト</span><br>
+        <span class="top_copy_sub">子どもたちのよりよい成長のために</span>
+      </p>
+      <div class="top_image_author">
+        <a class="author" href="https://pixabay.com/ja/users/qimono-1962238/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3255136">Arek Socha</a>
+        による<br>
+        <a class="author" href="https://pixabay.com/ja/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3255136">Pixabay</a>
+        からの画像
+      </div>
+
+    </div>
     <div class="kind_contents"></div>
     <div class="reason_contents"></div>
     <div class="register_way_contens"></div>
