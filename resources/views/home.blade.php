2be899b30e7c50353ef001995a317dde58b9e3fb
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-23 17:22:30 +0900

    kind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.common')
 @section('content')
   <div class="wrapper">
+
     <div id="top_image">
       <div id="top_icon"></div>
       <p class="top_copy">
@@ -14,9 +15,38 @@
         <a class="author" href="https://pixabay.com/ja/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3255136">Pixabay</a>
         からの画像
       </div>
-
     </div>
-    <div class="kind_contents"></div>
+
+    <div class="kind_contents">
+      <div class="explanation">
+        <span class="explain_head">どんなアンケートをするの？</span><br>
+        <span class="explain_body">
+          この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
+          この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。
+          この文章はダミーです。文字の大きさ、
+        </span>
+      </div>
+      <ul class="kind_images">
+        <li class="kind_image">
+          <p class="kind_head">〇〇アンケート</p>
+          <div id="kind_image1"></div>
+        </li>
+        <li class="kind_image">
+          <p class="kind_head">△△アンケート</p>
+          <div id="kind_image2"></div>
+        </li>
+        <li class="kind_image">
+          <p class="kind_head">□□アンケート</p>
+          <div id="kind_image3"></div>
+        </li>
+        <li class="kind_image">
+          <p class="kind_head">◇◇アンケート</p>
+          <div id="kind_image4"></div>
+        </li>
+      </ul>
+    </div>
+
+
     <div class="reason_contents"></div>
     <div class="register_way_contens"></div>
     <div class="advantage_contents"></div>
