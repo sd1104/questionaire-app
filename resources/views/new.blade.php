277fa7015271faf0bcd228bd047095a74ed65478
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-25 19:32:31 +0900

    view2

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -5,36 +5,41 @@
 
   <div class="formbody1">
     <p class="form_title">ログイン</p>
-    <div class="question_box">
-      <div class="question">
-        <p>問○</p>
-        <p>
-          サンプルです。サンプルです。サンプルです。
-          サンプルです。サンプルです。サンプルです。
-          サンプルです。サンプルです。サンプルです。
-          サンプルです。サンプルです。サンプルです。
-        </p>
+    <form action="get" class="user_form">
+      <div class="question_box">
+        <div class="question">
+          <p>問○</p>
+          <p>
+            サンプルです。サンプルです。サンプルです。
+            サンプルです。サンプルです。サンプルです。
+            サンプルです。サンプルです。サンプルです。
+            サンプルです。サンプルです。サンプルです。
+          </p>
+        </div>
+        <div class="answer">
+          <input type="checkbox">
+          <div class="select">
+            選択肢
+          </div>
+          <input type="checkbox">
+          <div class="select">
+            選択肢
+          </div>
+          <input type="checkbox">
+          <div class="select">
+            選択肢
+          </div>
+          <input type="checkbox">
+          <div class="select">
+            選択肢
+          </div>
+        </div>
+
+        <div class="action">
+          <input type="submit" class="btn submit_btn" value="確認画面へ">
+        </div>
       </div>
-      <div class="answer">
-        <input type="checkbox">
-        <div class="select">
-          選択肢
-        </div>
-        <input type="checkbox">
-        <div class="select">
-          選択肢
-        </div>
-        <input type="checkbox">
-        <div class="select">
-          選択肢
-        </div>
-        <input type="checkbox">
-        <div class="select">
-          選択肢
-        </div>
-      </div>
-      <input type="submit" class="btn submit_btn" value="確認画面へ">
-    </div>
+    </form>
   </div>
 
 </div>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('new');
+    return view('confirm');
 });
 
 // Route::get('index', function () {
