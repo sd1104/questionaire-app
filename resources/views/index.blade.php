777aa747662edc84b01cd5b01d9e0d8d05747839
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-25 18:05:26 +0900

    progress1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,55 +3,53 @@
 
 <div class="wrapper">
 
-  <table class="table table-responsive">
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-          </td>
-          <td>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-          </td>
-          <td>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-          </td>
-          <td>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-            Mark <br>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+  <table>
+    <thead>
+      <tr>
+        <th>アンケート一覧</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          アンケートⅠ
+        </td>
+        <td>
+          アンケートⅡ
+        </td>
+        <td>
+          アンケートⅢ
+        </td>
+        <td>
+          アンケートⅣ
+        </td>
+      <tr>
+        <td class="q_box">
+          <div class="q_cont">
+            <div class="q_name">
+              <p>アンケート１</p>
+              <p>(○月■日〜○月■日)</p>
+            </div>
+            <div class="q_btn">
+              <button class="answer_btn">回答済み</button>
+            </div>
+          </div>
+        </td>
+        <td>
+          アンケート1<br>
+
+        </td>
+        <td>
+          アンケート1<br>
+
+        </td>
+        <td>
+          アンケート1<br>
+
+        </td>
+      </tr>
+      </tr>
+    </tbody>
   </table>
 
 </div>
