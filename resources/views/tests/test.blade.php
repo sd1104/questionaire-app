33ec938107e917ef4292887465b2d2cc4c62fa5e
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-05 12:33:35 +0900

    Create test.blade.php

---
+++ resources/views/tests/test.blade.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/app.css">
+  <title>Test.test.blade.php</title>
+</head>
+<body>
+
+  <div>
+    <h2>Hello New World</h2>
+    <p>Hello! This is Test Page.</p>
+  </div>
+
+</body>
+</html>
