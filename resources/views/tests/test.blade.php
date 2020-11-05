b194f2b608b52fe6aea342d4a89076965aa1d460
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-05 13:24:19 +0900

    Update test.blade.php

--- resources/views/tests/test.blade.php
+++ resources/views/tests/test.blade.php
@@ -13,10 +13,19 @@
   <title>Test.test.blade.php</title>
 </head>
 <body>
-  <?php if($pageFlag !== 0): ?>
+  <?php if($pageFlag === 0): ?>
     <div>
       <h2>Hello New World</h2>
       <p>Hello! This is Test Page.</p>
+      @foreach($values as $value)
+        {{ $value->id}}
+        <br>
+        {{ $value->name }}
+        <br>
+        {{ $value->age }}
+        <br>
+        {{ $value->hobby }}
+      @endforeach
     </div>
   <?php endif; ?>
 </body>
