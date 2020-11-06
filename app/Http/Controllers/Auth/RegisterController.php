f923a2e378f290290359a2750b78df34f119279f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-06 19:18:53 +0900

    place redirected_to changed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,8 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = 'contact/index';
 
     /**
      * Create a new controller instance.
