48e80fbf78b4a73a20a222620ccfe0d55c8dc1e7
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-20 11:44:20 +0900

    Update 2020_11_06_184133_create_users_table.php

--- database/migrations/2020_11_06_184133_create_users_table.php
+++ database/migrations/2020_11_06_184133_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name' ,50);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 8);
             $table->rememberToken();
             $table->timestamps();
         });
